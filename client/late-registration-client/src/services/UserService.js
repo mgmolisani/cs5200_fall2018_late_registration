4b74cc2e10b98a3897b3f2d354d89671eddb9dce
mgmolisani <molisani.m@husky.neu.edu>
2018-12-11 04:26:52 -0500

    user services need update to flatten pleeeeeease

--- client/late-registration-client/src/services/UserService.js
+++ client/late-registration-client/src/services/UserService.js
@@ -1,3 +1,5 @@
+import User from '../pages/User';
+
 const USER_API_URL = 'https://late-registration-server.herokuapp.com/user/';
 
 export const UserService = {
@@ -6,58 +8,28 @@ export const UserService = {
             .then(function (response) {
                 return response.json();
             });
+    },
+    createUser: user => {
+        return fetch(USER_API_URL + '/createUser', {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    },
+    deleteUser: userId => {
+        return fetch(USER_API_URL + '/deleteUserById/' + userId, {
+            method: 'DELETE'
+        })
+    },
+    updateUser: (userId, user) => {
+        return fetch(USER_API_URL + '/updateUser/' + userId, {
+            method: 'DELETE',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
     }
-// };
-//
-//     createCourse(course, callback) {
-//         return fetch(USER_API_URL, {
-//             method: 'POST',
-//             body: JSON.stringify(course),
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         }).then(function (response) {
-//             return response.json();
-//         }).then(callback);
-//     }
-//
-//     /**
-//      * Deletes a course by ID
-//      * @param courseId the course to delete
-//      * @param callback
-//      * @returns {Promise<Response>}
-//      */
-//     deleteCourse(courseId, callback) {
-//         return fetch(COURSE_API_URL + '/' + courseId, {
-//             method: 'DELETE'
-//         }).then(function (response) {
-//             if (response.ok) {
-//                 return response.text().then(callback);
-//             }
-//             return response.json().then(errorCallback);
-//         });
-//     }
-//
-//     /**
-//      * Updates a course
-//      * @param course the course to update (includes the ID)
-//      * @param callback
-//      * @returns {Promise<Response>}
-//      */
-//     updateCourse(course, callback) {
-//         return fetch(COURSE_API_URL, {
-//             method: 'PUT',
-//             body: JSON.stringify(course),
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         }).then(function (response) {
-//                 let responseJson = response.json();
-//                 if (response.ok) {
-//                     return responseJson.then(callback);
-//                 }
-//                 return responseJson.then(errorCallback);
-//             }
-//         );
-//     }
-}
+};
