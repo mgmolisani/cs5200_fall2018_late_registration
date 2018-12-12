d2a5ef4d25387202e5a743d1a5830690890b573a
shawnmartin <martin.sh@husky.neu.edu>
2018-12-11 22:53:44 -0500

    moved back to server

--- server/connection.js
+++ server/connection.js
@@ -3,41 +3,41 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const gameRoute = require('./routes/api/gameRoute');
-const postRoute = require('./routes/api/postRoute');
-const teamRoute = require('./routes/api/teamRoute');
-const userRoute = require('./routes/api/userRoute');
+const gameRoute = require('./src/routes/api/gameRoute');
+const postRoute = require('./src/routes/api/postRoute');
+const teamRoute = require('./src/routes/api/teamRoute');
+const userRoute = require('./src/routes/api/userRoute');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        ["http://localhost:3000"]);
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
+  res.header("Access-Control-Allow-Origin",
+      ["http://localhost:3000"]);
+  res.header("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
 });
 
 const connect = function () {
-    return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/late_registration', { useNewUrlParser: true });
+  return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/late_registration', { useNewUrlParser: true });
 };
 
 connect()
-    .then(() => {
-        app.use('/game', gameRoute);
-        app.use('/team', teamRoute);
-        app.use('/post', postRoute);
-        app.use('/user', userRoute);
+.then(() => {
+  app.use('/game', gameRoute);
+  app.use('/team', teamRoute);
+  app.use('/post', postRoute);
+  app.use('/user', userRoute);
 
-    });
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });
 
 module.exports = connect;
